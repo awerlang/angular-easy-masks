358e92fc19fbde512ea6c5313d069a0a4c8d5f08
André Werlang <beppe85@gmail.com>
2015-05-06 21:58:33 -0300

    refactor(tests): simplify unit tests

--- tests/maskProvider.spec.js
+++ tests/maskProvider.spec.js
@@ -1,5 +1,8 @@
+/* global inject */
+/// <reference path="../typings/jasmine/jasmine.d.ts"/>
 describe('provider', function () {
 
+    var easyMaskProvider;
     var easyMask;
     beforeEach(module('wt.easy'));
 
@@ -8,29 +11,29 @@ describe('provider', function () {
         var someMaskValue = '999.99';
 
         describe('can retrieve a published mask', function () {
-            var easyMaskProvider;
             beforeEach(function () {
                 module(function (_easyMaskProvider_) {
                     easyMaskProvider = _easyMaskProvider_;
                     easyMaskProvider.publishMask(someMaskName, someMaskValue);
                 });
             });
-
-            it('by name', function () {
-                inject(function (easyMask) {
-                    var mask = easyMask.getMask(someMaskName);
-                    expect(mask).toEqual(someMaskValue);
+            beforeEach(function () {
+                inject(function (_easyMask_) {
+                    easyMask = _easyMask_;
                 });
             });
 
-            it('the comparison is case insensitive', function () {
-                inject(function (easyMask) {
-                    var mask1 = easyMask.getMask(someMaskName.toLowerCase());
-                    expect(mask1).toEqual(someMaskValue);
+            it('by name', function () {
+                var mask = easyMask.getMask(someMaskName);
+                expect(mask).toEqual(someMaskValue);
+            });
 
-                    var mask2 = easyMask.getMask(someMaskName.toUpperCase());
-                    expect(mask2).toEqual(someMaskValue);
-                });
+            it('the comparison is case insensitive', function () {
+                var mask1 = easyMask.getMask(someMaskName.toLowerCase());
+                expect(mask1).toEqual(someMaskValue);
+
+                var mask2 = easyMask.getMask(someMaskName.toUpperCase());
+                expect(mask2).toEqual(someMaskValue);
             });
         });
     });
